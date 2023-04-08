5cce0a6251e841a5d35914a54f03a52081369537
Cecilia La Place <laplace95@gmail.com>
2023-04-08 13:35:37 -0700

    Minor clean up, session welcome page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, redirect, render_template, session, url_for#send_from_directory
 import json
 from os import environ as env
-
+import sqlite3
+import requests
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
@@ -10,6 +11,28 @@ ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
     
+'''
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_all():
+    conn = get_db_connection()
+    shark = conn.execute('SELECT * FROM sharks').fetchall()
+
+    conn.close()
+    return shark
+
+def get_shark(shark_id):
+    conn = get_db_connection()
+    shark = conn.execute('SELECT * FROM sharks WHERE id = ?',
+                        (shark_id,)).fetchone()
+    conn.close()
+    if shark is None:
+        print('No Sharks Here.')
+    return shark'''
+
 app = Flask(__name__)
 app.secret_key = env.get("APP_SECRET_KEY")
 
@@ -61,11 +84,8 @@ def index():
         sharks = json.load(f)
         sharks = sharks['features']
         print(sharks[0]['properties'])
-    return render_template('prevcards.html', sharks=sharks)
+    return render_template('prevcards.html', sharks=sharks, session=session)
 
-@app.route('/about')
-def about():
-    return render_template('test.html')
 
 @app.route('/card')
 def card():
@@ -75,3 +95,8 @@ def card():
         sharks = sharks['features']
         print(sharks[0]['properties'])
     return render_template('card.html', sharks=sharks)
+
+'''@app.route('/dbtest/<string:shark_id>')
+def post(shark_id):
+    shark = get_all()    
+    return render_template('rendertest.html', shark=shark)'''
